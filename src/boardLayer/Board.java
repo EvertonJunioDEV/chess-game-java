1d04d9a4296bc5aaca957ea38584e2ccec22d749
Everton <everton.junio132@gmail.com>
2024-02-16 14:19:57 -0300

    BoardException and defesive programming

--- src/boardLayer/Board.java
+++ src/boardLayer/Board.java
@@ -3,10 +3,13 @@ package boardLayer;
 public class Board {
 	private Integer rows;
 	private Integer columns;
-	
+
 	private Piece[][] pieces;
 
 	public Board(Integer rows, Integer columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at last 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		this.pieces = new Piece[rows][columns];
@@ -16,26 +19,45 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece(int rows, int columns) {
+		if (!positonExistes(rows, columns)) {
+			throw new BoardException("Error: position not on the board");
+		}
 		return pieces[rows][columns];
 	}
+
 	public Piece piece(Position position) {
+		if (!positonExistes(position)) {
+			throw new BoardException("Error: position not on the board");
+		}
 		return pieces[position.getRows()][position.getColumns()];
 	}
+
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Error: there is already a piece on position" + position);
+		}
 		pieces[position.getRows()][position.getColumns()] = piece;
 		piece.position = position;
 	}
+
+	private Boolean positonExistes(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public Boolean positonExistes(Position position) {
+		return positonExistes(position.getRows(), position.getColumns());
+	}
+
+	public Boolean thereIsAPiece(Position position) {
+		if (!positonExistes(position)) {
+			throw new BoardException("Error: position not on the board");
+		}
+		return piece(position) != null;
+	}
+
 }
---
+++ src/boardLayer/BoardException.java
@@ -0,0 +1,8 @@
+package boardLayer;
+
+public class BoardException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	BoardException(String msg){
+		super(msg);
+	}
+}
